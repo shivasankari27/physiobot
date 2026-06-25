c11f178a228aa7c01e56b4f7130539c62ac0d1e7
SHIVASANKARI V A <shivasankarivairamuthu@gmail.com>
2026-06-25 20:28:08 +0530

    Refactor SessionTracker class formatting

--- feedback/session.py
+++ feedback/session.py
@@ -1,18 +1,18 @@
-class SessionTracker:
-    def __init__(self):
-        self.history = []
-
-    def update(self, score):
-        self.history.append(score)
-        if len(self.history) > 30:
-            self.history.pop(0)
-
-    def trend(self):
-        if len(self.history) < 2:
-            return "stable"
-
-        if self.history[-1] > self.history[0]:
-            return "improving"
-        elif self.history[-1] < self.history[0]:
-            return "declining"
+class SessionTracker:  
+    def __init__(self):  
+        self.history = []  
+  
+    def update(self, score):  
+        self.history.append(score)  
+        if len(self.history) > 30:  
+            self.history.pop(0)  
+  
+    def trend(self):  
+        if len(self.history) < 2:  
+            return "stable"  
+  
+        if self.history[-1] > self.history[0]:  
+            return "improving"  
+        elif self.history[-1] < self.history[0]:  
+            return "declining"  
         return "stable"
